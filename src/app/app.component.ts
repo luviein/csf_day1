cb5f3808b8feef2745311fc5343997b98f55d7b3
Yenleng <yenlengggggg@gmail.com>
2023-08-01 14:00:09 +0800

    add toggling using [hidden]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'csf_01aug';
   updatedNumber: number;
+
+  leftEnable: boolean = true;
+  rightEnable: boolean = true;
+
   changedNumber(eventNumber: number) {
     this.updatedNumber = eventNumber;
     console.log("number received in parent: " + this.updatedNumber);
   }
+
+  ToggleLeft() {
+    this.leftEnable = !this.leftEnable;
+  }
+
+  ToggleRight() {
+    this.rightEnable = !this.rightEnable;
+  }
+
 }
