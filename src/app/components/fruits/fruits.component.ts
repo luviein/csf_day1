82556f2f1e9a56becd4563f451b9c940ee5d8560
Yenleng <yenlengggggg@gmail.com>
2023-08-01 14:22:59 +0800

    add ngFor loop

--- src/app/components/fruits/fruits.component.ts
+++ src/app/components/fruits/fruits.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fruits',
   templateUrl: './fruits.component.html',
   styleUrls: ['./fruits.component.css']
 })
-export class FruitsComponent {
+export class FruitsComponent implements OnInit {
+
   // Optional to put semicolon
 
-  fruit: string = "Durian";
+  fruit1: string = "Durian";
 
   fullName: string = "yenleng";
 
@@ -20,5 +21,14 @@ export class FruitsComponent {
     console.log("Button Test Clicked")
   };
 
-  
+  //instantiating array in JS complies to following structure below
+  fruits: {name: string, quantity: number}[] = [];
+
+  //manually push to populate array above
+  ngOnInit(): void {
+    this.fruits.push({name: "Mango", quantity: 10});
+    this.fruits.push({name: "Durian", quantity: 5});
+    this.fruits.push({name: "Avocado", quantity: 9});
+  }
+
 }
